66af75896e40254b4e4276f469d79934af33263d
MrAndrewLondon <MrAndrewLondon@gmail.com>
2017-05-22 22:32:40 +0100

    Challenge # 4

--- homework/challenge04.js
+++ homework/challenge04.js
@@ -8,3 +8,16 @@ shortcut("goodbye")  // --> "gdby"
 shortcut("nodegirls")  // --> "nd grls"
 shortcut('how are you today?') // --> "hw r y tdy?"
 */
+
+var string_result = "";
+function shortcut(string) {
+    var string_size = string.length;
+    for (var i = 0; i < string_size; i++) {
+        if (!((string[i] === "o") || (string[i] === "e") || (string[i] === "a") || (string[i] === "u") || (string[i] === "i"))) {
+            string_result += string[i];
+        }
+    }
+    console.log(string_result);
+}
+var given_string = "How are you today?";
+shortcut(given_string);
