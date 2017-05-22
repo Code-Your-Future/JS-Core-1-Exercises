5a62caa3beddc4523696bfc6bacad0c6a09dbb00
MHDRateb <ratebalissa@gmail.com>
2017-05-22 13:29:58 +0100

    change the solution from binary converter to series generator

--- homework/challenge05.js
+++ homework/challenge05.js
@@ -11,11 +11,22 @@ stringy(5) // --> '10101'
 
 The size will always be positive and will only use whole numbers.
 */
+
+var alter_fact = true;
 var warn = "Please enter postive number without floating point"
-var result = 0;
+var result = "";
 function stringy(size) {
     if (size > 0 && size != "" && size % 1 === 0) {
-        result = (size >>> 0).toString(2);
+        for (var i = 1; i <= size; i++) {
+            if (alter_fact) {
+                result += "1";
+                alter_fact = false;
+            }
+            else if (!alter_fact) {
+                result += "0";
+                alter_fact = true;
+            }
+        }
         console.log(result);
     }
     else {
@@ -23,4 +34,4 @@ function stringy(size) {
     }
 }
 
-stringy(3.5);
+stringy(6);
