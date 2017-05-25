5cbfd4537f08767f88490c1d6f9b903330703c08
Amir <amir.gevari@gmail.com>
2017-05-25 13:44:39 +0100

    I solved the Ones and Zeros challenge

--- homework/challenge05.js
+++ homework/challenge05.js
@@ -11,3 +11,19 @@ stringy(5) // --> '10101'
 
 The size will always be positive and will only use whole numbers.
 */
+var oneZero='';
+function stringy (size) {
+ for (i=1; i<= size; i++){
+ if ((size === 1) || (size===0)){
+ console.log(size());
+} else if (i % 2 === 1){
+    oneZero = "1" + oneZero 
+ }
+ else if (i % 2 === 0){
+     oneZero = "0" + oneZero
+ }
+}
+console.log(oneZero);
+};
+
+stringy (10);
