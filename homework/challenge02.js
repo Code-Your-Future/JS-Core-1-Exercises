89e035b61eb6bb9718b78785a90138300025ed77
coder <maali.alsahiuni@gmail.com>
2017-06-03 17:05:13 +0100

    jsweek1

--- homework/challenge02.js
+++ homework/challenge02.js
@@ -20,4 +20,19 @@ Counting to 15 should look like this:
 14
 'FizzBuzz'
 
-*/
+*/for (i=1; i<= 100; i++ ) {
+    if (i%15===0) {
+        console.log ("\'FizzBuzz\'");
+    } else if (i%5===0) {
+    console.log ("\'Buzz\'");
+    } else if (i%3===0) {
+        console.log ("\'Fizz\'");
+    } else {
+        console.log (i);
+    }
+    
+
+}
+     
+
+
--- homework/challenge06.js
+++ homework/challenge06.js
@@ -14,3 +14,27 @@ Running your function by passing in `alphabet` should give you an array of all c
 removeVowels(alphabet) // --> ['b', 'c', 'd', 'f', ... 'y', 'z']
 
 */
+
+var alphabet = ['a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y', 'z'];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+
+function removeVowels (alphaArray , vowelsArray) {
+    var newArray = [ ];
+    for (i=0; i< alphaArray.length; i++){
+        var isConsonant= true;
+        for (c=0; c<vowelsArray.length; c++){
+            if( alphaArray[i]=== vowelsArray[c]){
+                isConsonant = false;
+            } 
+        }
+        if (isConsonant){
+            newArray.push(alphaArray[i]);
+        }
+    }
+    return newArray;   
+}
+
+console.log(removeVowels(alphabet, vowels));
+
