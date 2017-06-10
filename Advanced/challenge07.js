df13efdf7f13006b8f0de9b5c55f4aaf4f58b6c4
kharazzoro <kha.1991.14.2017@gmail.com>
2017-06-10 15:14:50 +0100

    challenge 1-6-7 done

--- Advanced/challenge07.js
+++ Advanced/challenge07.js
@@ -1,17 +1,19 @@
 var bill = [7.99, 6.99, 14.99, 4.99];
 var numberOfFriends = 4;
 
-function calcalute(bill,tips){
+function calcalute(bill,numberOfFriends){
   total=0;
   for(var i=0;i<bill.length;i++){
     total+=bill[i];
   }
 
-  tip=(tips*total)/100;
-  total=total+tip;
-  eachone=(total/4);
-
+  // tip=(tips*total)/100;
+  // total=total+tip;
+  eachone=(total/numberOfFriends);
   console.log(eachone);
+  return eachone;
+
+
 }
 
-calcalute(bill,10);
+calcalute(bill,numberOfFriends);
---
+++ challenge06.js
@@ -0,0 +1,17 @@
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+var letters= ['a','b','c','d','e','f','g',
+'h','i','j','k','l','m','n','o','p','q','w','x','w','z'];
+function removeVowels(vowels,letters){
+  var updateLetters="";
+  for(var i=0;i<vowels.length;i++){
+
+      for(var n=0;n<letters.length;n++){
+
+
+      }
+
+  }
+  console.log(updateLetters);
+}
+
+removeVowels(vowels,letters);
