f1d651625ad1d1f02670fcfdbbd0367070d209e9
kharazzoro <kha.1991.14.2017@gmail.com>
2017-06-10 15:41:19 +0100

    Added tips

--- Advanced/challenge07.js
+++ Advanced/challenge07.js
@@ -1,19 +1,17 @@
 var bill = [7.99, 6.99, 14.99, 4.99];
 var numberOfFriends = 4;
-
-function calcalute(bill,numberOfFriends){
+var tips=10;
+function calcalute(bill,numberOfFriends,tips){
   total=0;
   for(var i=0;i<bill.length;i++){
     total+=bill[i];
   }
 
-  // tip=(tips*total)/100;
-  // total=total+tip;
+  totalTip=(tips*total)/100;
+  total+=totalTip;
   eachone=(total/numberOfFriends);
   console.log(eachone);
   return eachone;
-
-
 }
 
-calcalute(bill,numberOfFriends);
+calcalute(bill,numberOfFriends,tips);
