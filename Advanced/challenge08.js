55f9e561f6e563a7bbb1ce385900b9d353290d8f
kharazzoro <kha.1991.14.2017@gmail.com>
2017-06-10 15:54:38 +0100

    Calculate suzie owess

--- Advanced/challenge08.js
+++ Advanced/challenge08.js
@@ -1,9 +1,17 @@
-function Splitstuff(ketPrice,mugPrice){
+/* Challenge 8: Paying back a friend
+Nadia, Suzie and Katrina live together. They agreed to buy a kettle and three
+mugs, and split the cost.
+Nadia went to the shop and bought everything. Write a function to help her
+figure out how much Suzie and Katrina owe her.
+The function should accept the price of a kettle and the price of one of the
+mugs, then return the value Suzie owes.
+Test your function with different prices to see if it works.
+*/
+function splitStuff(ketPrice,mugPrice){
 
-var Allprice=ketPrice+(3*mugPrice);
-  var eachOne=Allprice/3;
-  var owe=eachOne*2;
-  return owe;
+var allPrice=ketPrice+(3*mugPrice);
+  var suzieOwe=allPrice/3;
+   return suzieOwe;
 }
-var SuzieOwe=Splitstuff(6,31);
-console.log(SuzieOwe);
+var suzieOwe=splitStuff(2,3);
+console.log(suzieOwe);
