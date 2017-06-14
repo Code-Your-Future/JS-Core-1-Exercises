e844e9e5861d455452f4d8e8178027a76a5e12d3
Anthonythuo <antonykariuki@yahoo.com>
2017-06-14 12:02:15 +0100

    my challenge03 homework

--- homework/challenge03.js
+++ homework/challenge03.js
@@ -16,11 +16,27 @@ Should return:
 
 'Found the needle at position 2'
 */
-var haystack = ['hay', 'rabbit', 'needle', 'hat'];
+/*var haystack = ['hay', 'rabbit', 'needle', 'hat'];
 for (var i = 0; i < haystack.length; i++) {
     if (haystack[i] === 'needle') {
         console.log('found the needle at position' + i)
     }
 }
+*/
+
+var haystack = ['hay', 'rabbit', 'needle', 'hat'];
+
+function findNeedle(haystack1) {
+    for (var i = 0; i < haystack1.length; i++) {
+        if (haystack1[i]==='needle'){
+            console.log('found the needle at position ' + i);
+        }
+    }
+   // if (haystack1[i] === true) {
+     //   console.log(false)
+    }// else console.log(true)
 
 
+//}
+
+findNeedle(haystack);
