2da5905fc1b2602efbeb0b2374391dc65911d274
OlenaKashuba <amely.jazz@gmail.com>
2017-10-30 10:32:44 +0000

    exercise practice

--- homework/challenge02.js
+++ homework/challenge02.js
@@ -1,6 +1,8 @@
 /* Challenge 2: FizzBuzz
 
-Write a for loop that prints the numbers from 1 to 100. But for multiples of 3 print “Fizz” instead of the number and for the multiples of 5 print “Buzz”. For numbers which are multiples of both 3 and 5 print “FizzBuzz”.
+Write a for loop that prints the numbers from 1 to 100. But for multiples of 3 print “Fizz” instead of
+he number and for the multiples of 5 print “Buzz”. For numbers which are multiples of both 3 
+and 5 print “FizzBuzz”.
 
 Counting to 15 should look like this:
 
--- main.js
+++ main.js
@@ -5,4 +5,101 @@
       node class-exercises.js
 */
 
-console.log('Hello World');
+//Console log Hello world 
+// console.log('Hello World');
+// console.log('I\'ve just started to learn Javascript');
+// console.log(5343);
+// console.log(typeof 55);
+// console.log(typeof true);
+// console.log(typeof 'Hello'); //comment
+
+// var b;
+// console.log(typeof b);
+
+//console.log(1/0);
+
+
+// IF ELSE STATEMENT 
+
+var isHappy = false;
+if (isHappy === true) {
+  // if I am happy, print "Hello world!"
+  console.log('Hello world!');
+} else {
+  // if I am sad, print a frowny face
+  console.log(':(');
+}
+
+
+// EVEN ODD + -
+
+// for (var i=1; i <=10; i++) {
+// 	console.log(evenodd(i));
+// }
+
+// var number = -5;
+
+// if ((number%2) === 0 && number>0) {
+// 	console.log('positive even');
+// } else if ((number%2) === 0 && number<0) {
+// 	console.log('negative even');
+// } else if ((number%2) === 1 && number>0) {
+// 	console.log('positive odd');
+// } else if (number === 0) {
+// 	console.log ('Zero');
+// } else {
+// 	console.log('negative odd');
+// }
+
+
+//  LOOP
+
+
+// for (var i=2; i <= 10; i+=2) {
+//   console.log(i);
+// }
+
+// for (var i=5; i <= 100; i+=5) {
+//   console.log(i);
+// }
+
+// for (var i=1; i <= 100; i++) {
+// 	if (i%5 === 0) {
+// 		console.log(i);
+// 	}
+// }
+
+
+// LOOP SUM
+
+
+// var sum = 0;
+// for (var i=1; i<=100; i++) {
+// 	sum = sum + i;
+// }
+// console.log(sum);
+
+
+
+//FUNCTIONS 
+
+
+function multiply (x, y) {
+  return x*y;
+}
+
+console.log(multiply(2, 3));
+
+
+function limits (x, y) {
+	var sum = 0;
+  	for (var i=x; i<=y; i++) {
+		// sum += i;
+		sum = sum + i;
+	}
+	return sum;
+}
+
+console.log(limits(1,10));
+
+
