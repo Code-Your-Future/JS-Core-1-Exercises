2724fa221e49e2e3b070373adc8cfcdf8944e746
yevhensydorov <assid@ukr.net>
2017-10-30 12:35:24 +0000

    did 3 challenges from homework

--- homework/challenge01.js
+++ homework/challenge01.js
@@ -39,3 +39,50 @@ var writers = [
 
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
+
+var writers = [
+  {
+    firstName: "Virginia",
+    lastName: "Woolf",
+    occupation: "writer",
+    age: 59,
+    alive: false
+  },
+  {
+    firstName: "Zadie",
+    lastName: "Smith",
+    occupation: "writer",
+    age: 41,
+    alive: true
+  },
+  {
+    firstName: "Jane",
+    lastName: "Austen",
+    occupation: "writer",
+    age: 41,
+    alive: false
+  },
+  {
+    firstName: "bell",
+    lastName: "hooks",
+    occupation: "writer",
+    age: 64,
+    alive: true
+  },
+];
+
+console.log("Base exercise");
+console.log("================================================");
+
+writers.forEach(function(writer) {
+  console.log("Hi, my name is " + writer.firstName + " " + writer.lastName + ". I am " + writer.age + " years old, and work as a " + writer.occupation);
+});
+
+console.log("Advanced exercise");
+console.log("================================================");
+
+writers.forEach(function(writer) {
+  if (writer.alive) {
+    console.log("Hi, my name is " + writer.firstName + " " + writer.lastName + ". I am " + writer.age + " years old, and work as a " + writer.occupation);
+  }
+});
