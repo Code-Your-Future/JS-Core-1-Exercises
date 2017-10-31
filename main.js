c7416d06832dc04770f157387ca51ba31d5f4cae
Edwigesalomeseri <edwige1st@gmail.com>
2017-10-31 20:13:59 +0000

    exercise

--- main.js
+++ main.js
@@ -5,4 +5,50 @@
       node class-exercises.js
 */
 
-console.log('Hello World');
+// console.log('Hello World');
+// console.log("Hello code your future");
+// console.log('1234');
+
+
+// var x;
+//  x = "22" ;
+//  console.log( x);
+
+//  var city = "glasgow";
+//  console.log (city);
+
+// var number = 4;
+// var isNumberOdd = (number%2)
+
+//  if (isNumberOdd) {
+//   console.log('odd');
+// } else {
+//   console.log('even');
+// }
+
+//  for (var i = 1; i < 100; i=i+1) {
+//    console.log (i);
+//   // i += "2, 4, 6, 8, 10... " + i + "<br>"
+//   var isNumbereven = (i%2===0);
+//   if (isNumbereven) {
+//    console.log('even');
+//    }
+//  }
+
+
+function multiply(x,y){
+  var multiply= x * y;
+   console.log(multiply);
+ }
+
+ multiply(2,3);
+
+ function limits (x,y){
+   var sum = 0;
+   for (var i=x; i<=y; i++){
+     sum += i;
+   }
+   console.log(sum); 
+  }
+limits(1,10));
+
