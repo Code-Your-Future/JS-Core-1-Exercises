7c00d64042643d29f8195c0a881e243937dd60d5
Ibramoha <ibramohauk@gmail.com>
2017-11-01 02:30:33 +0000

    challenge03 completed

--- homework/challenge03.js
+++ homework/challenge03.js
@@ -4,7 +4,7 @@ Can you find the needle in the haystack?
 
 Write a function `findNeedle()` that takes an array full of junk but containing one "needle", which you need to find with a for loop.
 
-After your function finds the needle it should return a message (as a string) that says: `"Found the needle at position x"`, with `x` being the index number at which you find the needle.
+After your function finds the needle it should return a mess"Found the needle at position x"`, with `x` being the index number at which you find the needle.age (as a string) that says: `
 
 So:
 
@@ -16,3 +16,17 @@ Should return:
 
 'Found the needle at position 2'
 */
+
+var haystack = ['hay', 'rabbit', 'needle', 'hat'];
+function findNeedle(arr) {
+
+    for (var i = 0; i <= arr.length - 1; i++) {
+		if(arr[i] === "needle") {
+			console.log('Found the needle at position ' + i);
+		}
+}
+}
+
+
+findNeedle(haystack);
+
