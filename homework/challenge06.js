26e712e232a13d2a01ac91f6a2c89a94c7f7478c
monaazami <32145698+monaazami@users.noreply.github.com>
2017-11-03 09:18:44 +0000

    complited the task

--- homework/challenge06.js
+++ homework/challenge06.js
@@ -17,5 +17,14 @@ removeVowels(alphabet) // --> ['b', 'c', 'd', 'f', ... 'y', 'z']
 function removeVowels(){
     var vowel = ["a","e", "i","o", "u"];
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    
+    for (i=0,i<28,++i);
+    if (i=a|e|i|o|u);{
+        i.remove(i);
+       
+    }
+    else {
+        console.log(i);
+    }
 }
+
+removeVowels();
