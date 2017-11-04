236738f8de3196abc80b57eb12caeeb99dd103a9
OlenaKashuba <amely.jazz@gmail.com>
2017-11-04 12:04:27 +0000

    Ali ex5 ex6

--- homework/ali/ex4.js
+++ homework/ali/ex4.js
@@ -3,13 +3,13 @@
 // If it is one of the CYF cities, log a success message (e.g. "Well done!"),
 //  if it is not log a different message (.e.g "Not a CYF city, try again!").
 
+
 function CYFcity(city) {
 	if (city === 'London' || city === 'Manchester' || city === 'Glasgow') {
 		console.log ('Well done!'); 
 	} else { 
 		console.log ('Not a CYF city, try again!');
 	}
-	return
 }
 
 CYFcity('London');
--- homework/ali/ex5.js
+++ homework/ali/ex5.js
@@ -2,3 +2,29 @@
 // The new function should take an array of city names and checks if
 //  all of them are CYF cities, some of them are CYF cities or none of them are CYF cities
 //   (Hint: use a loop).
+
+var cyfCities = ['London', 'Manchester', 'Glasgow'];
+
+function checkCity(array) {
+	var counter = 0;
+	for (var i = 0; i < array.length; i++) {
+	 for (var j = 0; j < cyfCities.length; j++) {
+	 	if (array[i] === cyfCities[j]) {
+	 		counter += 1;
+	 	}
+	 }
+	} 
+	if (counter === array.length) {
+		console.log('All of the cities are CYF cities');
+	} else if (counter > 0) {
+		console.log('Some of the cities are CYF cities');
+	} else {
+		console.log ('None of them is a CYF city, try again!');
+	}
+}
+
+
+checkCity(['London', 'Manchester', 'Glasgow']);
+checkCity(['London', 'Paris', 'Berlin']);
+checkCity(['Kiev', 'Berlin', 'Paris', 'Tallin']);
+
