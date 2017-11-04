b5414ce939acf849fd21cc50a26f9c380ee84398
Ibramoha <ibramohauk@gmail.com>
2017-11-04 00:17:19 +0000

    challenge 08

--- homework/advanced/challenge08.js
+++ homework/advanced/challenge08.js
@@ -11,3 +11,23 @@ mugs, then return the value Suzie owes.
 
 Test your function with different prices to see if it works.
 */
+
+var kettlePrice;
+var mugPrice;
+var suziemoney;
+
+function suziePayback(kettlePrice,mugPrice) {
+
+    var share = (kettlePrice + (3*mugPrice))/3;
+
+    return suziemoney = 2*share;
+
+
+}
+
+console.log("Suzie owes " + Math.floor(suziePayback(6,8))+ " £ to Nadia and Katrina");
+console.log("Suzie owes " + Math.floor(suziePayback(25,9)) + " £ to Nadia and Katrina");
+console.log("Suzie owes " + Math.floor(suziePayback(19,51)) + " £ to Nadia and Katrina");
+console.log("Suzie owes " + Math.floor(suziePayback(7,16)) + " £ to Nadia and Katrina");
+
+
