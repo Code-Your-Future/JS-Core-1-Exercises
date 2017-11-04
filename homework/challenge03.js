e95f17561471ce6a6e15c83f0f7a3baf526544e4
Ameerkabir <ameerkabir2@gmail.com>
2017-11-04 23:09:11 +0000

    altered issues from reviews on github

--- homework/challenge03.js
+++ homework/challenge03.js
@@ -7,16 +7,22 @@ Write a function `findNeedle()` that takes an array full of junk but containing 
 After your function finds the needle it should return a message (as a string) that says: `"Found the needle at position x"`, with `x` being the index number at which you find the needle.
 
 So:*/
-function findNeedle(){
-
 
 var haystack = ['hay', 'rabbit', 'needle', 'hat'];
 
+function findNeedle(){
 
-     for( var i= haystack[2]; i< haystack.length; i++);{
-         console.log('Found the needle at postion 2');
+
+
+
+     for( var i= 0; i< haystack.length; i++){
+         if(haystack[i]=== 'needle'){
+
+         
+         console.log('Found the needle at postion' + i);
+         }
      }
  }
 
- findNeedle();
+ findNeedle(haystack);
 
