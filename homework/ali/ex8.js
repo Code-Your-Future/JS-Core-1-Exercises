5a2a2da4ba381069b2c61bcdca1391af26c01741
OlenaKashuba <amely.jazz@gmail.com>
2017-11-09 23:21:39 +0000

    Finish challenges week 1

--- homework/ali/ex8.js
+++ homework/ali/ex8.js
@@ -2,3 +2,16 @@
 // with a randomly generated integer number (Hint: try Math.random()).
 //  If it does match it logs out with a success message (e.g. "Good guess!").
 //  If their guess does not match, log a different message (e.g. "Incorrect, try again!").
+
+function int0to10(int) {
+	var random = Math.floor(Math.random() * 11);
+	console.log(random);
+	if (random === int) {
+		console.log("Good guess!!");
+	} else {
+		console.log("Incorrect, try again!");
+	}
+}
+
+int0to10(10);
+
