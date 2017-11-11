b133d797b48e03eaaa09d28f17a037f19306a5c2
gdgfjfjnvvaa <gdgdhd@gmail.com>
2017-11-11 12:40:13 +0000

    Update of Ali challenge exercices 6 with another solution

--- post-challenges .js
+++ post-challenges .js
@@ -2,11 +2,11 @@
 subtraction, multiplication and division. The log should be formatted like: "For numbers {firstNumber} and {secondNumber}, 
 the addition is {addition}, the subtraction is {subtraction}, the multiplication is {multiplication} and the division is {division}". */
 
-function arithmeticOperators(firstNumber, secondNumber){
+function arithmeticOperators(firstNumber, secondNumber) {
     console.log('Addition: ' + (firstNumber + secondNumber) +
-   '\nSubstration: ' + (firstNumber - secondNumber) +
-   '\nMultiplication: ' + (firstNumber * secondNumber) +
-   '\nDivision: '+ (firstNumber / secondNumber));
+        '\nSubstration: ' + (firstNumber - secondNumber) +
+        '\nMultiplication: ' + (firstNumber * secondNumber) +
+        '\nDivision: ' + (firstNumber / secondNumber));
 }
 arithmeticOperators(7, 4);
 arithmeticOperators(4, 8.5);
@@ -21,20 +21,20 @@ B if the mark is in the range 70 - 79, C if the mark is in the range 60 - 69, D 
 and E if the mark is 49 or lower. 
 *************************************************************************************************************************/
 
-function myGrade(mark){
-    if(mark >= 90 && mark <= 100){
+function myGrade(mark) {
+    if (mark >= 90 && mark <= 100) {
         console.log('Your grade is: A*')
-    }else if(mark >= 80 && mark <= 89){
+    } else if (mark >= 80 && mark <= 89) {
         console.log('Your grade is: A')
-    }else if(mark >= 70 && mark <= 79){
+    } else if (mark >= 70 && mark <= 79) {
         console.log('Your grade is: B')
-    }else if(mark >= 60 && mark <= 69){
+    } else if (mark >= 60 && mark <= 69) {
         console.log('Your grade is: C')
-    }else if(mark >= 50 && mark <= 59){
+    } else if (mark >= 50 && mark <= 59) {
         console.log('Your grade is: D')
-    }else if(mark <= 49){
+    } else if (mark <= 49) {
         console.log('Your grade is: E')
-    }else {
+    } else {
         console.log('Hmm... may be your are a genius!')
     }
 }
@@ -47,15 +47,15 @@ myGrade(70);
 3.Write a function which takes an integer number and checks if it is within 20 digits of 100 or within 20 digits of 400. 
 *************************************************************************************************************************/
 
-function areYouWithinTwentyDigitsOf(integer){
-    if((100-integer) <= 20 || (120 -integer) <= 20){
+function areYouWithinTwentyDigitsOf(integer) {
+    if ((100 - integer) <= 20 || (120 - integer) <= 20) {
         console.log(true)
-    }else if ((400-integer) <= 20 || (420 -integer) <= 20){
+    } else if ((400 - integer) <= 20 || (420 - integer) <= 20) {
         console.log(true)
-    }else {
+    } else {
         console.log(false);
-    }    
-    
+    }
+
 }
 
 areYouWithinTwentyDigitsOf(49);
@@ -70,15 +70,16 @@ areYouWithinTwentyDigitsOf(390);
 
 var cyfCities = ['London', 'Glascow', 'Manchester'];
 
-function whereToFindCyf(cityName){
-    for (var cyfCitiesIndex = 0; cyfCitiesIndex < cyfCities.length; cyfCitiesIndex++){
-        if(cityName === cyfCities[cyfCitiesIndex]){
+function whereToFindCyf(cityName) {
+    for (var cyfCitiesIndex = 0; cyfCitiesIndex < cyfCities.length; cyfCitiesIndex++) {
+        if (cityName === cyfCities[cyfCitiesIndex]) {
             console.log('Well done CYF! ' + cityName + ' is one of the Code Your Future cities');
-          break;
+            break;
         }
-        }if  (cityName !== cyfCities[cyfCitiesIndex]){
-            console.log(cityName + ' is not a CYF city, try again!');
-        }
+    }
+    if (cityName !== cyfCities[cyfCitiesIndex]) {
+        console.log(cityName + ' is not a CYF city, try again!');
+    }
 }
 
 
@@ -91,36 +92,36 @@ whereToFindCyf('Glascow');
                                                    Another method
 *************************************************************************************************************************/
 
-function whereToFindCyfArr(cityName){
+function whereToFindCyfArr(cityName) {
     var cyfCities = ['London', 'Glascow', 'Manchester'];
-    for (var cyfCitiesIndex = 0; cyfCitiesIndex < cyfCities.length; cyfCitiesIndex++){
-        if(cityName.indexOf(cyfCitiesIndex) !== -1){
+    for (var cyfCitiesIndex = 0; cyfCitiesIndex < cyfCities.length; cyfCitiesIndex++) {
+        if (cityName.indexOf(cyfCitiesIndex) !== -1) {
             console.log('yes');
-        } 
-            
-    }
-}  
+        }
 
-   whereToFindCyfArr(['Glascow', 'malabo', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);       
+    }
+}
+
+whereToFindCyfArr(['Glascow', 'malabo', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);
 
 
 /* ************************************************************************************************************************
                                                    Another method
 *************************************************************************************************************************/
-   
 
-function whereToFindCyf(cityName){
+
+function whereToFindCyf(cityName) {
     var cyfCities = ['London', 'Glasgow', 'Manchester'];
     var city = false;
-    cyfCities.forEach(function(item){
-        if(cityName === item){
+    cyfCities.forEach(function(item) {
+        if (cityName === item) {
             return city = true;
-        }  
+        }
     });
-    if(city){
+    if (city) {
         console.log('Well done! ' + cityName + ' is one of the Code Your Future cities');
-    }else {
-        console.log(cityName + ' is not a CYF city, try again!');    
+    } else {
+        console.log(cityName + ' is not a CYF city, try again!');
     }
 }
 
@@ -138,108 +139,137 @@ The new function should take an array of city names and checks if all of them ar
 some of them are CYF cities or none of them are CYF cities (Hint: use a loop).
 *************************************************************************************************************************/
 
-function whereToFindCyfArr(cityName){
+function whereToFindCyfArr(cityName) {
     var cyfCities = ['London', 'Glascow', 'Manchester'];
-    for (var cityNameIndex = 0; cityNameIndex < cityName.length; cityNameIndex++){
-        for (var cyfCitiesIndex = 0; cyfCitiesIndex < cyfCities.length; cyfCitiesIndex++){
-            if(cityName[cityNameIndex] === cyfCities[cyfCitiesIndex]){
+    for (var cityNameIndex = 0; cityNameIndex < cityName.length; cityNameIndex++) {
+        for (var cyfCitiesIndex = 0; cyfCitiesIndex < cyfCities.length; cyfCitiesIndex++) {
+            if (cityName[cityNameIndex] === cyfCities[cyfCitiesIndex]) {
                 console.log('Well done CYF! ' + cityName[cityNameIndex] + ' is one of the Code Your Future cities');
-                         break;  
-       }
-            }if(cityName[cityNameIndex] !== cyfCities[cyfCitiesIndex]){
-                    console.log(cityName[cityNameIndex]+ ' is not a CYF city, try again!');               } 
-            
+                break;
+            }
+        }
+        if (cityName[cityNameIndex] !== cyfCities[cyfCitiesIndex]) {
+            console.log(cityName[cityNameIndex] + ' is not a CYF city, try again!');
+        }
+
     }
-}  
+}
 
-   whereToFindCyfArr(['Glascow', 'malabo', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);  
+whereToFindCyfArr(['Glascow', 'malabo', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);
 
-   
-   /* ************************************************************************************************************************
+
+/* ************************************************************************************************************************
                                                    Another method
 *************************************************************************************************************************/
 
 
-function whereToFindCyf(cityName){
+function whereToFindCyf(cityName) {
     var matchCities = [];
     var cyfCities = ['London', 'Glasgow', 'Manchester'];
-    for (var i = 0; i < cyfCities.length; i++){
-        for (var j = 0; j < cityName.length; j++){
-            if (cityName[j] === cyfCities[i]){
+    for (var i = 0; i < cyfCities.length; i++) {
+        for (var j = 0; j < cityName.length; j++) {
+            if (cityName[j] === cyfCities[i]) {
                 matchCities.push(cityName[j]);
             }
-        } 
-    }   if (matchCities.length === cyfCities.length && cityName.length === cyfCities.length){
-            console.log('Yes all!');
-        } else if ( matchCities.length <= cyfCities.length  && matchCities.length > 0){
-            console.log('Yes some!');
-        } else {
-            console.log('None!');
-        } 
+        }
+    }
+    if (matchCities.length === cyfCities.length && cityName.length === cyfCities.length) {
+        console.log('Yes all!');
+    } else if (matchCities.length <= cyfCities.length && matchCities.length > 0) {
+        console.log('Yes some!');
+    } else {
+        console.log('None!');
+    }
 }
 
 whereToFindCyf(['Glasgow', 'Malabo', 'Kolofata', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);
 
 
 
-   /* ************************************************************************************************************************
+/* ************************************************************************************************************************
                                                    Another method
 *************************************************************************************************************************/
 
 
-function whereToFindCyfArr(cityName){
+function whereToFindCyfArr(cityName) {
     var cyfCities = ['London', 'Glascow', 'Manchester'];
-    
-    function targetCityName(theTarget){
+
+    function targetCityName(theTarget) {
         return cyfCities.indexOf(theTarget) === -1;
     }
-    
+
     var filtered = cityName.filter(targetCityName);
     console.log(filtered);
-    
-    if (filtered.length === cityName.length){
+
+    if (filtered.length === cityName.length) {
         console.log('None');
-    }   else if (filtered.length === 0){
+    } else if (filtered.length === 0) {
         console.log('All');
-    }   else {
+    } else {
         console.log('Some');
     }
 }
 
-whereToFindCyfArr(['Glascow', 'Malabo', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);  
+whereToFindCyfArr(['Glascow', 'Malabo', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);
 
-   
-   /* ************************************************************************************************************************
+
+/* ************************************************************************************************************************
 6-Extend the function from Exercise 5 by copy/pasting it to a new function. 
 The new function should handle UPPER and lower case city names in the input (Hint: try .toLowerCase()).
 *************************************************************************************************************************/
 
-function whereToFindCyfArrCase(cityName){
+function whereToFindCyfArrCase(cityName) {
     var cyfCities = ['London', 'Glascow', 'Manchester'];
-    for (var cityNameIndex = 0; cityNameIndex < cityName.length; cityNameIndex++){
-        for (var cyfCitiesIndex = 0; cyfCitiesIndex < cyfCities.length; cyfCitiesIndex++){
-            if(cityName[cityNameIndex].toLowerCase() === cyfCities[cyfCitiesIndex].toLowerCase()){
+    for (var cityNameIndex = 0; cityNameIndex < cityName.length; cityNameIndex++) {
+        for (var cyfCitiesIndex = 0; cyfCitiesIndex < cyfCities.length; cyfCitiesIndex++) {
+            if (cityName[cityNameIndex].toLowerCase() === cyfCities[cyfCitiesIndex].toLowerCase()) {
                 console.log('Well done CYF! ' + cityName[cityNameIndex].toLowerCase() + ' is one of the Code Your Future cities');
-                         break;  
-       }
-        }   if(cityName[cityNameIndex] !== cyfCities[cyfCitiesIndex]){
-                    console.log(cityName[cityNameIndex].toLowerCase() + ' is not a CYF city, try again!');               
-        } 
-            
+                break;
+            }
+        }
+        if (cityName[cityNameIndex] !== cyfCities[cyfCitiesIndex]) {
+            console.log(cityName[cityNameIndex].toLowerCase() + ' is not a CYF city, try again!');
+        }
+
     }
-}  
+}
 
-whereToFindCyfArrCase(['Glascow', 'malabo', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);  
+whereToFindCyfArrCase(['Glascow', 'malabo', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);
 
- /* ************************************************************************************************************************
+/* ************************************************************************************************************************
+                                                   Another method
+*************************************************************************************************************************/
+
+function whereToFindCyfArr(cityName) {
+    var cyfCities = ['London', 'Glascow', 'Manchester'].join(', ').toLowerCase().split(' ');
+
+    function targetCityName(theTarget) {
+        return cyfCities.indexOf(theTarget) === -1;
+    }
+
+    var filtered = cityName.join(', ').toLowerCase().split(' ').filter(targetCityName);
+    console.log(filtered);
+
+    if (filtered.length === cityName.length) {
+        console.log('None');
+    } else if (filtered.length === 0) {
+        console.log('All');
+    } else {
+        console.log('Some');
+    }
+}
+whereToFindCyfArr(['Glascow', 'Malabo', 'Manchester', 'Paris', 'London', 'Marseille', 'Lyon']);
+
+/* ************************************************************************************************************************
 7-Write a function which appends a character to the beginning of a given string, 
 repeated a given number of times. So for example, yourFunction('ello', 3, 'h') would log 'hhhello'.
 *************************************************************************************************************************/
-function addCharToBegin(string, time, character){
+function addCharToBegin(string, time, character) {
     var array = string.split('');
-    for(var i = 0; i < time; i++){
+    for (var i = 0; i < time; i++) {
         array.unshift(character);
-    } console.log(array.join(''));
+    }
+    console.log(array.join(''));
 }
 addCharToBegin('ello', 3, 'h');
 
@@ -249,11 +279,11 @@ number (Hint: try Math.random()). If it does match it logs out with a success me
 If their guess does not match, log a different message (e.g. "Incorrect, try again!").
 *************************************************************************************************************************/
 
-function guestGame(yourNumber){
+function guestGame(yourNumber) {
     var randomNumber = Math.floor(Math.random * 99999002);
-    if(yourNumber === randomNumber){
+    if (yourNumber === randomNumber) {
         console.log('Good guest');
-    }else{
+    } else {
         console.log("Incorrect, try again!");
     }
 }
@@ -265,18 +295,18 @@ guestGame(2223);
  The new function should now take an array of numbers as guesses, and tells the user if any of their guesses were correct.
 *************************************************************************************************************************/
 
-function guestGame(arrNumber){
+function guestGame(arrNumber) {
     var randomNumber = Math.floor(Math.random * 99999002);
-    for(var i = 0; i < arrNumber.length; i++) {
-        if(arrNumber[i] === randomNumber){
-            console.log( arrNumber[i] + ' a good guest');
-        }else{
+    for (var i = 0; i < arrNumber.length; i++) {
+        if (arrNumber[i] === randomNumber) {
+            console.log(arrNumber[i] + ' a good guest');
+        } else {
             console.log(arrNumber[i] + " is incorrect, try again!");
         }
     }
 }
 
-guestGame([34, 45, 78,1003,46]);
+guestGame([34, 45, 78, 1003, 46]);
 
 
 
@@ -284,12 +314,12 @@ guestGame([34, 45, 78,1003,46]);
 10.Write a function which reverses the string given as an argument.
 *************************************************************************************************************************/
 
-function reverse (str){
+function reverse(str) {
     var anotherString = '';
-    for(i = str.length - 1; i >= 0; i-- ){
+    for (i = str.length - 1; i >= 0; i--) {
         anotherString += str[i];
     }
     console.log(anotherString);
 }
 
-reverse ('code your future');
+reverse('code your future');
