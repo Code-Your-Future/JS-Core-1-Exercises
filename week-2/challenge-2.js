6d132585ce64538f909c898e17d916b5e22a83fe
harun <harunk3uk@gmail.com>
2018-03-18 21:21:25 +0000

    I finished this tree exersize

--- week-2/challenge-2.js
+++ week-2/challenge-2.js
@@ -11,10 +11,69 @@ http://www.mathsisfun.com/pascals-triangle.html
 - Else ini­tial­ize it with the sum of the ele­ments from pre­vi­ous row
 
 */
-
 function pascal(num) {
-  // TODO
-  
+  var array = {},
+    cash1 = 0,
+    cash2 = 0; // TODO
+  for (i = 0; i < num; i++) {
+    array[i] = [];
+    for (a = 0; a < i + 1; a++) {
+      if (a === 0 || a === i) {
+        array[i][a] = 1;
+      } else {
+        cash1 = array[i - 1][a - 1];
+        cash2 = array[i - 1][a];
+        array[i][a] = cash1 + cash2;
+      }
+    }
+  }
+  return array;
+}
+/*
+function print(array, num) {
+  var arr = {};
+
+  var numCash = num.splice();
+  for (i = 0; i < num; i++) {
+    arr[i] = [];
+    for (j = num - i; j === 0; j--) {
+      arr[i][j] = "   ";
+    }
+    for (k = 0; k < i * 2 + 1; k++) {
+      if (array[i][k] < 10) {
+      }
+      if (array[i][k] / 10 < 10 && array[i][k] / 10 > 1) {
+      }
+    }
+  }
+}*/
+function print(array, num) {
+  var cashArray = {};
+  for (i = 0; i < num; i++) {
+    cashArray[i] = [];
+  }
+  cashArray = array;
+  for (i = 0; i < num; i++) {
+    for (a = 0; a < i + 1; a++) {
+      if (cashArray[i][a] < 10) {
+        cashArray[i][a] = " " + cashArray[i][a] + " ";
+      } else if (cashArray[i][a] < 100 && cashArray[i][a] >= 10) {
+        cashArray[i][a] = " " + cashArray[i][a];
+      } else {
+        cashArray[i][a] = "" + cashArray[i][a] + "";
+      }
+    }
+  }
+  for (i = 0; i < num; i++) {
+    cashArray[i] = cashArray[i].join("   ");
+  }
+
+  for (i = 0; i < num; i++) {
+    for (k = 0; k < num - i - 1; k++) {
+      cashArray[i] = "   " + cashArray[i];
+    }
+  }
+  return cashArray;
 }
 
-console.log(pascal(5));
+console.log(print(pascal(13), 13));
